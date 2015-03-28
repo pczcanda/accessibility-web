69ff9839377f64c9b3cb91032f0875c34711c6e1
Paciencia Zinga Canda <p.canda.pc@googlemail.com>
2015-03-28 16:21:32 +0000

    initial form styles for template

---
+++ assets/js/jquery.pczcanda.accessibility.js
@@ -0,0 +1,34 @@
+﻿window.ACCESSIBILITY = (function (module, $) {
+  "use strict";
+
+  if (!window.console) {
+    window.console = {
+      log: function () {
+      },
+      info: function () {
+      },
+      debug: function () {
+      }
+    }
+  }
+
+  if (typeof module.config === 'undefined') {
+    module.config = {};
+  }
+
+  module.access = (function (module, $) {
+    var submodule = {};
+
+    submodule.init = function () {
+    };
+
+    $(document).ready(function () {
+      submodule.init();
+    });
+
+    return submodule;
+  }(module, $));
+
+  return module;
+
+}(window.ACCESSIBILITY || {}, window.jQuery));
